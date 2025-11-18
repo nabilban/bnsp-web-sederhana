f60a89d332c72f211b69e7ac43d397ca904886bb
nabilban <nabilban354@gmail.com>
2025-11-18 22:31:53 +0700

    Remove redundant comment in app.js and add author name in index.html

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = 8080;
 
-// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
